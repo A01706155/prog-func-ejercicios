1fa72659bda5e5a27a18ff0696ae1e0fce900475
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-03-14 21:22:08 -0600

    Problemas finalizados

--- problemario1.rkt
+++ problemario1.rkt
@@ -1,28 +1,28 @@
 #lang racket
 
-; ##########################
-; # Ejercicios Daniel Cruz #
-; ##########################
+;; ##########################
+;; # Ejercicios Daniel Cruz #
+;; ##########################
 
-; 1. fahrenheit-to-celsius 
+;; 1. fahrenheit-to-celsius 
 ;toma como entrada una temperaturafen grados Fahrenheit y laconvierte a su equivalente en grados Celsius 
 (define (fahrenheit-to-celsius f)
   (/ (* 5 (- f 32)) 9) ;;formula
 )
 
-; 2. sign
+;; 2. sign
 ;regresa 1 si el numero es positivo y -1 si es negativo
 (define (sign n)
  (if (>= n 0) 1 -1) 
   )
 
-; 3. roots
+;; 3. roots
 ;devuelve la raiz que resuelve la ecuacion cuadratica
 (define (roots a b c)
   (/ (+ (- b) (sqr (- (* b b) (* 4 a c)))) (* 2 a))
   )
 
-; 4. bm1
+;; 4. bm1
 ;devuelve el indice de masa corporal a partir del peso y la altura
 ;tambien devuelve condiciones de peso basadas en el resultado
 (define (bmi w h)
@@ -36,7 +36,7 @@
     )
   )
 
-; 5. factorial
+;; 5. factorial
 ;toma un entero positivo y devuelve su factorial
 (define (factorial n)
   (cond
@@ -44,13 +44,13 @@
     [else (* n (factorial(- n 1)))])
   )
 
-; 7. pow
+;; 7. pow
 ;dados 2 numeros a y b devuelve el resultado de a elevado a la potencia b
 (define (pow a b)
   (expt a b)
   )
 
-; 16. average
+;; 16. average
 ;devuelve el promedio de una lista dada como parametro
 (define (sum lst)
 (if (null? lst) 0
@@ -62,11 +62,11 @@
       (/ (sum lst) (length lst)))
   )
  
-; #############################
-; # Ejercicios Manolo Ramírez #
-; #############################
+;; #############################
+;; # Ejercicios Manolo Ramírez #
+;; #############################
 
-; 6. duplicate
+;; 6. duplicate
 (define (duplicate lst)
   (cond
     [(null? lst) '()]
@@ -74,50 +74,57 @@
     )
 )
 
-; 8. fib
+;; 8. fib
 (define (fib n)
   (if (<= n 1)
       1 (+ (fib (- n 1)) (fib (- n 2)))
   )
 )
 
-;10 positives
+;; 10 positives
 (define (positives lst)
     (cond
       [(null? lst) '()]
       [(>= (car lst) 0) (my-append (cons(car lst) '()) (positives (cdr lst)))]
       [else (positives (cdr lst))]))
 
-; 11. add-list
+;; 11. add-list
 (define (add-list lst)
     (if (empty? lst) 0 (+ (car lst) 
         (add-list (cdr lst)))
     )
 )
 
-; 12. invert-pairs
+;; 12. invert-pairs
 (define (invert-pairs lst)
   (map (lambda (lst2) (reverse lst2)) lst
   )
 )
 
-; 15. dot-product
+;; 15. dot-product
 (define (dot-product a b)
   (cond
-    ( (null? a) 0)
+    ((null? a) 0)
         (else
          (+ (* (car a) (car b)) (dot-product (cdr a) (cdr b)))
         )
     )
 )
 
+;; 20. binary
+(define (binary n)
+  (cond ((zero? n) '())
+        (else (cons(remainder n 2) (dec->bin (quotient n 2))
+            )
+        )
+  )
+)
 
-;
 
 
-
-; ------------------------------------------------------------
-; Partes de código que nos pueden ayudar mucho, sesión 1 y 2
+; .------------------------------------------------------------.
+; | Partes de código que nos pueden ayudar mucho, sesión 1 y 2 |
+; .------------------------------------------------------------.
 (define repeat-head
   (lambda (atom amount)
     (cond
@@ -132,4 +139,3 @@
       [else (cons (car lst1)
                   (my-append (cdr lst1) lst2))])))
 ;; example: (my-append '(1 2) '(3 4)) => '(1 2 3 4)
-; ------------------------------------------------------------
